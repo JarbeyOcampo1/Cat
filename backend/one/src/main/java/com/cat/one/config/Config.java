f94898b628204f097aa5a0dca3b1e47b8fe4cbd9
JarbeyOcampo1 <164644585+JarbeyOcampo1@users.noreply.github.com>
2025-01-27 18:51:34 -0500

    Update Config.java

--- backend/one/src/main/java/com/cat/one/config/Config.java
+++ backend/one/src/main/java/com/cat/one/config/Config.java
@@ -4,18 +4,19 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.lang.NonNull;
 
 // Configuracion de la API
 @Configuration
 @EnableWebMvc
-public class Config implements WebMvcConfigurer {
-    // Metodo para configurar el CORS
+public class Config  implements WebMvcConfigurer {
+    //Metodo para configurar el CORS
     @Override
-    public void addCorsMappings(@NonNull CorsRegistry registry) {
+      public void addCorsMappings(CorsRegistry registry){
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173/") // Escribir la url del proyecto front a conectar
-                .allowedMethods("*") // Podemos especificar los metodos a permitir desde el front
-                .allowCredentials(true); // Para que permita las credenciales
+        .allowedOrigins("http://localhost:5173/")//Escribir la url del proyecto front a conectar
+        .allowedMethods("*")//podemos especificar los metodos a permitir desde el front
+        .allowCredentials(true);//para que permita las credenciales
+    
     }
+    
 }
